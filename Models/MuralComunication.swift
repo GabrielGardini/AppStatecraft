116c0ab80770633252f4b66d09c0155aae7a7038
Larissa <larissa@10-40-55-197.0200.ecps.br.iosda.org>
2025-06-10 11:15:46 -0300

    remove arquivos ignorados

---
+++ Models/MuralComunication.swift
@@ -0,0 +1,8 @@
+//
+//  MuralComunication.swift
+//  ifish
+//
+//  Created by Larissa on 10/06/25.
+//
+
+import Foundation
---
+++ Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  ifish
+//
+//  Created by Larissa on 10/06/25.
+//
+
+import Foundation
