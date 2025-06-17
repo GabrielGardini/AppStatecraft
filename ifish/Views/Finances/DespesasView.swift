e1c8e5c795edb511579db9f1437440388a72fffc
bel <isabelmarras13@gmail.com>
2025-06-17 11:28:26 -0300

    despesas view

---
+++ ifish/Views/Finances/DespesasView.swift
@@ -0,0 +1,19 @@
+//
+//  DespesasView.swift
+//  ifish
+//
+//  Created by Aluno 21 on 17/06/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct DespesasView: View{
+    
+    var body: some View{
+        VStack(){
+            Text("Teste")
+                .font(.title)
+        }
+    }
+}
