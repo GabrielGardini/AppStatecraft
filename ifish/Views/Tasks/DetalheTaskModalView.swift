672bd5c30f0db1d6153244ba962d6e021bad0a84
Lari <l177154@dac.unicamp.br>
2025-06-26 11:44:15 -0300

    Cria arquivo DetalheTaskModalView

---
+++ ifish/Views/Tasks/DetalheTaskModalView.swift
@@ -0,0 +1,20 @@
+//
+//  DetalheTaskModalVIew.swift
+//  ifish
+//
+//  Created by Larissa on 26/06/25.
+//
+
+import SwiftUI
+
+struct DetalheTaskModalVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetalheTaskModalVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        DetalheTaskModalVIew()
+    }
+}
